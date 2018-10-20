21a3a966002548da8994bf0aaf093a05b9e1a90f
zy <endsofjava@163.com>
2018-10-20 09:54:35 +0800

    新增Customer

---
+++ src/main/java/com/zy/domain/Customer.java
@@ -0,0 +1,25 @@
+package com.zy.domain;
+
+/**
+ * project:  demo
+ *
+ * @author: zy
+ * @date: 2018/10/20  09 : 52
+ */
+public class Customer {
+    private String name;
+    private Integer age;
+    private String nickname;
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+}
