28d2bd82271c9fa6a14d20b690089048f185faad
zy <endsofjava@163.com>
2018-10-20 09:55:59 +0800

    新增Customer

---
+++ src/main/java/com/zy/domain/Customer.java
@@ -0,0 +1,25 @@
+package com.zy.domain;
+
+/**
+ * project:  demo
+ *
+ * @author: zy
+ * @date: 2018/10/20  09 : 53
+ */
+public class Customer {
+    private String username;
+    private double age;
+    private String nickName;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setAge(double age) {
+        this.age = age;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+}
